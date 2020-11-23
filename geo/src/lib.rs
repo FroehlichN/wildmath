4c58c826f6312d8e905eb1ad474ac64a71db3ca8
Norbert Fröhlich <norbert.froehlich@alice.de>
2020-11-23 23:40:54 +0100

    Add function to check if point is on a line

--- geo/src/lib.rs
+++ geo/src/lib.rs
@@ -1,9 +1,22 @@
 use num::Zero;
+use std::ops::{Mul, Add};
 
 /// Represents a 2D point
 pub struct TwoPoint<T> {
     x : T,
     y : T,
+}
+
+impl<T> TwoPoint<T>
+where
+    T: Mul<T, Output = T>,
+    T: Add<T, Output = T>,
+    T: Zero,
+{
+    pub fn lies_on(self, l: TwoLine<T>) -> bool {
+        let r = l.a * self.x + l.b * self.y + l.c;
+        r.is_zero()
+    }
 }
 
 /// Represents a 2D line a*x+b*y+c=0
@@ -33,10 +46,19 @@ impl<T: Clone + Zero> TwoLine<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num::rational::Ratio;
 
     #[test]
     #[should_panic]
     fn invalid_two_line() {
         TwoLine::new(0,0,1);
+    }
+    #[test]
+    fn point_lies_on_line() {
+        let a = TwoPoint {x: Ratio::new( 6, 1),
+            y: Ratio::new( -3, 2)};
+        let l = TwoLine::new( Ratio::new(1, 1),
+            Ratio::new(2, 1), Ratio::new(-3, 1) );
+        assert!(a.lies_on(l));
     }
 }
