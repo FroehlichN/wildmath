ed40eeac1d16ed68bbca0130d4a5a1fb345c6e06
Norbert Fröhlich <norbert.froehlich@alice.de>
2020-12-17 21:43:15 +0100

    Replace baby equation and rational equation with linear equation

--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -1,37 +1,5 @@
 use num::{Num};
 
-/// Represents the equation x + m = n, x=?
-pub struct BabyEq<T> {
-    m: T,
-    n: T,
-}
-
-impl<T> BabyEq<T>
-where
-    T: Num,
-    T: Copy,
-{
-    fn x(&self) -> T {
-        self.n - self.m
-    }
-}
-
-/// Represents the equation m*x = n, x=?
-pub struct RatEq<T> {
-    m: T,
-    n: T,
-}
-
-impl<T> RatEq<T>
-where
-    T: Num,
-    T: Copy,
-{
-    fn x(&self) -> T {
-        self.n/self.m
-    }
-}
-
 enum LinSummand<T> {
     Number(T),
     Unknown(T),
@@ -75,22 +43,26 @@ mod tests {
 
     #[test]
     fn xplus2eq7() {
-        let eq1 = BabyEq {m: 2, n: 7};
+        let eq1 = LinEq {lhs: vec![LinSummand::Unknown(1), LinSummand::Number(2)],
+            rhs: vec![LinSummand::Number(7)]};
         assert_eq!(eq1.x(), 5);
     }
     #[test]
     fn xplus10eq2() {
-        let eq1 = BabyEq {m: 10, n: 2};
+        let eq1 = LinEq {lhs: vec![LinSummand::Unknown(1), LinSummand::Number(10)],
+            rhs: vec![LinSummand::Number(2)]};
         assert_eq!(eq1.x(), -8);
     }
     #[test]
     fn threexeq12() {
-        let eq1 = RatEq {m: 3, n: 12};
+        let eq1 = LinEq {lhs: vec![LinSummand::Unknown(3)],
+            rhs: vec![LinSummand::Number(12)]};
         assert_eq!(eq1.x(), 4);
     }
     #[test]
     fn fivexeq11() {
-        let eq1 = RatEq {m: Ratio::new(5, 1), n: Ratio::new(11, 1) };
+        let eq1 = LinEq {lhs: vec![LinSummand::Unknown(Ratio::new(5, 1))],
+            rhs: vec![LinSummand::Number(Ratio::new(11, 1))]};
         assert_eq!(eq1.x(), Ratio::new(11, 5));
     }
     #[test]
