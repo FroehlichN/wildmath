3b29d8117a646558b9f66098f06bff172c58bb79
Norbert Fröhlich <norbert.froehlich@alice.de>
2020-12-18 14:25:30 +0100

    Calculating with terms in x

--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -1,6 +1,7 @@
 use num::{Num};
-use std::ops::{Div};
+use std::ops::{Div, Mul, Add, Sub, Neg};
 
+#[derive(Debug)]
 enum LinSummand<T> {
     Number(T),
     Unknown(T),
@@ -38,6 +39,7 @@ where
 }
 
 /// Represents a linear term in x
+#[derive(Debug)]
 pub struct LinTermInX<T> {
     summands: Vec<LinSummand<T>>,
 }
@@ -60,6 +62,103 @@ where
         }
 
         LinTermInX {summands: q}
+    }
+}
+
+impl<T> Mul<T> for LinTermInX<T>
+where
+    T: Num,
+    T: Copy,
+{
+    type Output = LinTermInX<T>;
+
+    fn mul(self, other: T) -> LinTermInX<T> {
+        let mut p: Vec<LinSummand<T>> = Vec::new();
+
+        for (_, s) in self.summands.iter().enumerate() {
+            match s {
+                LinSummand::Number(a) => p.push( LinSummand::Number(*a * other) ),
+                LinSummand::Unknown(b) => p.push( LinSummand::Unknown(*b * other) ),
+            }
+        }
+
+        LinTermInX {summands: p}
+    }
+}
+
+impl<T> Add<T> for LinTermInX<T>
+where
+    T: Num,
+    T: Copy,
+{
+    type Output = LinTermInX<T>;
+
+    fn add(self, other: T) -> LinTermInX<T> {
+        let mut s: Vec<LinSummand<T>> = self.summands;
+        s.push( LinSummand::Number(other) );
+        LinTermInX {summands: s}
+    }
+}
+
+impl<T> Sub<T> for LinTermInX<T>
+where
+    T: Num,
+    T: Neg<Output = T>,
+    T: Copy,
+{
+    type Output = LinTermInX<T>;
+
+    fn sub(self, other: T) -> LinTermInX<T> {
+        let mut d: Vec<LinSummand<T>> = self.summands;
+        d.push( LinSummand::Number(-other) );
+        LinTermInX {summands: d}
+    }
+}
+
+impl<T> Sub<LinSummand<T>> for LinTermInX<T>
+where
+    T: Num,
+    T: Neg<Output = T>,
+    T: Copy,
+{
+    type Output = LinTermInX<T>;
+
+    fn sub(self, other: LinSummand<T>) -> LinTermInX<T> {
+        let mut d: Vec<LinSummand<T>> = self.summands;
+        match other {
+            LinSummand::Number(a) => d.push( LinSummand::Number(-a) ),
+            LinSummand::Unknown(b) => d.push( LinSummand::Unknown(-b) ),
+        }
+        LinTermInX {summands: d}
+    }
+}
+
+impl<T> PartialEq for LinTermInX<T>
+where
+    T: Num,
+    T: Copy,
+{
+    fn eq(&self, other: &Self) -> bool {
+        let mut m = T::zero();
+        let mut n = T::zero();
+        let mut o = T::zero();
+        let mut p = T::zero();
+
+        for (_, s) in self.summands.iter().enumerate() {
+            match s {
+                LinSummand::Number(a) => m = m + *a,
+                LinSummand::Unknown(b) => n = n + *b,
+            }
+        }
+
+        for (_, s) in other.summands.iter().enumerate() {
+            match s {
+                LinSummand::Number(a) => o = o + *a,
+                LinSummand::Unknown(b) => p = p + *b,
+            }
+        }
+
+        return (m == o) && (n == p);
     }
 }
 
@@ -106,5 +205,16 @@ mod tests {
             LinSummand::Number(Ratio::new(5, 1))]} / Ratio::new(7, 1);
         let eq1 = LinEq {lhs: lhs.summands, rhs: rhs.summands};
         assert_eq!(eq1.x(), Ratio::new(17, 36));
+    }
+    #[test]
+    fn arithmatic_with_terms_in_x() {
+        let t1 = LinTermInX {summands: vec![LinSummand::Unknown(1),
+            LinSummand::Number(3)]};
+        let t2 = t1 * 2;
+        let t3 = t2 + 10;
+        let t4 = t3 - 4;
+        let t5 = t4 / 2;
+        let t6 = t5 - LinSummand::Unknown(1);
+        assert_eq!(t6, LinTermInX {summands: vec![LinSummand::Number(6)]});
     }
 }
