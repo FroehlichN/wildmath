2fd6929c7830c7256d63b3f7b48917a17f71edd2
Norbert Fröhlich <norbert.froehlich@alice.de>
2023-07-24 21:23:01 +0200

    Move out red projective geometry into own library

--- projectivegeored/src/lib.rs
+++ projectivegeored/src/lib.rs
@@ -55,9 +55,9 @@ where
         let y2 = other.x.b.clone();
 
         let n = x1.clone() * y2.clone() - x2.clone() * y1.clone();
-        let numer = n.clone() * n;
-        let d1 = x1.clone() * x1 + y1.clone() * y1;
-        let d2 = x2.clone() * x2 + y2.clone() * y2;
+        let numer = T::zero() - n.clone() * n;
+        let d1 = x1.clone() * x1 - y1.clone() * y1;
+        let d2 = x2.clone() * x2 - y2.clone() * y2;
         return numer / (d1*d2);
     }
 }
@@ -75,7 +75,7 @@ where
         let y1 = self.x.b.clone();
         let x2 = other.x.a.clone();
         let y2 = other.x.b.clone();
-        let r = x1*x2+y1*y2;
+        let r = x1*x2-y1*y2;
         r.is_zero()
     }
 }
@@ -266,82 +266,29 @@ mod tests {
     use super::*;
     use num::rational::Ratio;
     #[test]
-    fn one_dimensional_euclidean_projective_quadrance() {
+    fn one_dimensional_relativistic_projective_quadrance() {
         let a1 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(1,1));
         let a2 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(2,1));
-        assert_eq!(a1.quadrance(&a2),Ratio::new(1,2));
+        assert_eq!(a1.quadrance(&a2),Ratio::new(25,24));
     }
     #[test]
-    fn one_dimensional_euclidean_projective_quadrance_2() {
-        let a1 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(2,1));
-        let a2 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(4,1));
-        let q = Ratio::new(1,1)-Ratio::new(11*11,1)/Ratio::new(13*17,1);
-        assert_eq!(a1.quadrance(&a2),q);
+    fn one_dimensional_relativistic_projective_perpendicularity() {
+        let a1 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(1,1));
+        let a2 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(-1,1));
+        assert!(a1.is_perpendicular(&a1));
+        assert!(a2.is_perpendicular(&a2));
     }
     #[test]
-    fn one_dimensional_euclidean_projective_perpendicularity() {
-        let a1 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(4,1));
-        let a2 = ProjOnePoint::new(Ratio::new(-8,1),Ratio::new(6,1));
-        assert!(a1.is_perpendicular(&a2));
-    }
-    #[test]
-    fn one_dimensional_euclidean_projective_perpendicularity_2() {
-        let a1 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(0,1));
-        let a2 = ProjOnePoint::new(Ratio::new(0,1),Ratio::new(1,1));
-        assert!(a1.is_perpendicular(&a2));
-    }
-    #[test]
-    fn one_dimensional_euclidean_projective_perpendicularity_3() {
-        let a1 = ProjOnePoint::new(Ratio::new(2,3),Ratio::new(1,5));
-        let a2 = ProjOnePoint::new(Ratio::new(-3,1),Ratio::new(10,1));
-        assert!(a1.is_perpendicular(&a2));
-    }
-    #[test]
-    fn rotation_of_one_dimensional_projective_points() {
-        let a1 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(0,1));
-        let a2 = ProjOnePoint::new(Ratio::new( 3,1),Ratio::new( 4,1));
-        let a3 = ProjOnePoint::new(Ratio::new( 0,1),Ratio::new( 1,1));
-        let a4 = ProjOnePoint::new(Ratio::new(-4,1),Ratio::new( 3,1));
-        let r = Rotation::new(Ratio::new( 3,1),Ratio::new( 4,1));
-        assert_eq!(a1.clone()*r.clone(),a2.clone());
-        assert_eq!(a3.clone()*r,a4.clone());
-        assert_eq!(a1.quadrance(&a3),a2.quadrance(&a4));
-    }
-    #[test]
-    fn rotation_of_one_dimensional_projective_points_2() {
+    fn one_dimensional_relativistic_projective_quadrance_2() {
         let a1 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(2,1));
-        let a2 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(1,1));
-        let a3 = ProjOnePoint::new(Ratio::new(-2,1),Ratio::new(1,1));
-        let r = Rotation::new(Ratio::new(-1,1),Ratio::new(1,1));
-        assert_eq!(a1.clone()*r.clone(),a2.clone());
-        assert_eq!(a2*r,a3);
+        let a2 = ProjOnePoint::new(Ratio::new(2,1),Ratio::new(-3,1));
+        assert_eq!(a1.quadrance(&a2),Ratio::new(-49,15));
     }
     #[test]
-    fn multiplication_of_rotations_of_one_dimensional_projective_points() {
-        let a1 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(5,1));
-        let r1 = Rotation::new(Ratio::new( 7,1),Ratio::new( 4,1));
-        let r2 = Rotation::new(Ratio::new( 11,1),Ratio::new( 4,1));
-        assert_eq!((a1.clone()*r1.clone())*r2.clone(),a1*(r1*r2));
-    }
-    #[test]
-    fn reflection_of_one_dimensional_projective_points() {
-        let a1 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(0,1));
-        let a2 = ProjOnePoint::new(Ratio::new(-1,1),Ratio::new(1,1));
-        let a3 = ProjOnePoint::new(Ratio::new(0,1),Ratio::new(1,1));
-        let a4 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(1,1));
-        let a5 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(2,1));
-        let a6 = ProjOnePoint::new(Ratio::new(1,1),Ratio::new(3,1));
-        let a7 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(1,1));
-        let a8 = ProjOnePoint::new(Ratio::new(-1,1),Ratio::new(2,1));
-
-        let s = Reflection::new(Ratio::new(-1,1),Ratio::new(1,1));
-
-        assert_eq!(a1.clone()*s.clone(),a2.clone());
-        assert_eq!(a3.clone()*s.clone(),a4.clone());
-        assert_eq!(a2.clone()*s.clone(),a1.clone());
-        assert_eq!(a4.clone()*s.clone(),a3.clone());
-        assert_eq!(a5.clone()*s.clone(),a6.clone());
-        assert_eq!(a7.clone()*s.clone(),a8.clone());
+    fn one_dimensional_relativistic_projective_quadrance_3() {
+        let a1 = ProjOnePoint::new(Ratio::new(3,1),Ratio::new(1,1));
+        let a2 = ProjOnePoint::new(Ratio::new(2,1),Ratio::new(-3,1));
+        assert_eq!(a1.quadrance(&a2),Ratio::new(121,40));
     }
 }
 
