135d62f8e59e896685f8f7e2e31d097e496a0a66
Norbert Fröhlich <norbert.froehlich@alice.de>
2024-03-20 21:43:21 +0100

    Point lies on plane for n dimensions

---
+++ affinegeo/src/lib.rs
@@ -0,0 +1,87 @@
+/*
+Copyright 2024 Norbert Fröhlich
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+use num::{Zero};
+use std::ops::{Mul, Add};
+use linalg::{ColumnVector, RowVector};
+
+
+
+/// Represents an n-D point
+#[derive(Debug, Clone)]
+pub struct Point<T> {
+    coords: Vec<T>,
+}
+
+impl<T> Point<T>
+{
+    pub fn new(coords: Vec<T>) -> Point<T> {
+        Point { coords: coords }
+    }
+}
+
+impl<T> Point<T>
+where
+    T: Zero,
+    T: PartialEq,
+    T: Add,
+    T: Mul<Output = T>,
+    T: Clone,
+{
+    pub fn lies_on(&self, pi: &Plane<T>) -> bool {
+        let pointv = RowVector::new(self.coords.clone());
+        let planev = ColumnVector::new(pi.coords.clone());
+        let r = pointv * planev;
+        r == pi.d.clone()
+    }
+}
+
+
+/// Represents an n-D plane
+/// a1*x1 + a2*x2 + a3*x3 + ... = d
+#[derive(Debug, Clone)]
+pub struct Plane<T> {
+    coords: Vec<T>,
+    d: T,
+}
+
+impl<T> Plane<T>
+{
+    pub fn new(coords: Vec<T>, d: T) -> Plane<T> {
+        Plane { coords: coords, d: d }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn points_lie_on_a_plane() {
+        let p1 = Point::new(vec![3,0,0]);
+        let p2 = Point::new(vec![0,1,0]);
+        let p3 = Point::new(vec![0,0,2]);
+        let pi = Plane::new(vec![2,6,3],6);
+        assert!(p1.lies_on(&pi));
+        assert!(p2.lies_on(&pi));
+        assert!(p3.lies_on(&pi));
+    }
+
+}
+
